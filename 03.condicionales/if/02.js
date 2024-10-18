196aca30067f3b75256397ab05c860e7c3b7afa9
josejd2 <josejd2.jlv@gmail.com>
2024-10-18 13:46:24 +0200

    ejercicio 2 condicionales

--- 03.condicionales/if/02.js
+++ 03.condicionales/if/02.js
@@ -1,7 +1,12 @@
-let num = Number(prompt("Introduce un número: "));
+// Un programa que pida un número y diga si es positivo o negativo. El cero se considera positivo
 
-if (num >= 0){
+// Pedimos el número:
+let num = Number(prompt("Introduce un número:"));
+// TODO: habría que comprobar que efectivamente recibimos un número en el paso anterior y en caso de que no, podemos devolver un mensaje o acabar el programa
+
+// Comprobamos si el número es positivo o negativo:
+if(num >= 0){
     alert("Es positivo")
-}else{
+} else {
     alert("Es negativo")
 }
